4693be6b922fff52369a527b458ef16af3a9ec76
Reinhard Zitzmann <reinhard.zitzmann@gmail.com>
2024-06-18 09:50:05 +0200

    refactor: improve formatting of Order struct debug output

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ pub struct Order {
 impl Debug for Order {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&format!(
-            "{}: {:12} @ {:7}",
+            "{}: {:12} @ {:<7}",
             self.order_id, self.order_qty, self.limit_price
         ))
     }
